b8b0560920d5e423710c022f23e55e17b7b4e40b
mxdavis <md01@me.com>
2018-01-30 08:19:08 +0200

    organize into routes and services folders

---
+++ services/passport.js
@@ -0,0 +1,17 @@
+const passport = require('passport')
+const keys = require('../config/keys.js')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(accessToken)
+    }
+  )
+)
